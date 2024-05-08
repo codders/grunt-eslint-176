dbc16d9188e94d57089d833712665e0968803ba8
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-05-08 10:32:24 +0200

    Inital commit

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+/* eslint-env node */
+
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-eslint' );
+
+	grunt.initConfig( {
+		eslint: {
+			options: {
+				cache: true
+			},
+			all: [
+				'**/*.{js,json}',
+				'!Gruntfile.js',
+				'!modules/gadget-skip.js',
+				'!tests/coverage/**',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		},
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
